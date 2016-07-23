3b5f6a518806f34b3bc13f1f596d0eb53c766de3
kaushiksk21 <kaushikskalmady@gmail.com>
2016-07-24 01:19:10 +0530

    More optional arguments
    -f to specify formats
    -x to exclude
    
    Plus neater presentation

--- imgrab.sh
+++ imgrab.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-usage() 
+usage()
 {
-echo "Usage: $0 [optional arguments] [url]"
-echo "Command line options:"
-echo "    -o  DIR     Save all pictures in DIR directory(by default all pictures are saved in the current directory)"
+echo -e "\nUsage: $0 [OPTIONS] [url]"
+echo -e "\nDownloads all images from a specified url.
+Default formats downloaded : png,jpg,jpeg,gif,tif,bmp"
+echo -e "\nOPTIONS:"
 echo "    -h          Print this help menu"
-echo "Example: Download all the pictures and save them in the user input directory"
+echo "    -o  DIR     Save all images in the directory DIR
+                (by default all images are saved in a sub-directory in the current directory)"
+echo "    -f \"ext1 ext2 ext3 ..\"     Download specified formats/extensions only"
+echo "    -x \"ext1 ext2 ext3 ..\"     Exclude specified formats/extensions and download the rest"
+echo -e "\nEXAMPLES:
+    Download all the images and save them in the user input directory"
 echo "    $0 -o ~/my/input/dir [url]"
+echo -e "\n    Download only png images and save them in the user input directory"
+echo "    $0 -o ~/my/input/dir -f \"png\" [url]"
+echo -e "\n    Download all images except png and jpg"
+echo "    $0 -x \"png jpg\" [url]"
+echo "    (This will save the images in a sub-dir [url-images] in the current directory)"
 }
 
+#DEFAULTS
+otrue=0
+ftrue=0
+xtrue=0
+DATETIME="`date +%Y%m%d%_H%M`"
+j=0
+total_size=0
 
-
-#echo "$COMMAND_LINE_OPTIONS_HELP"
-INPUT_DIR="`pwd`"
-# parse the options
-while getopts 'o:h' opt ; do
-  case $opt in
-    h) echo "$COMMAND_LINE_OPTIONS_HELP"
-       exit 
-    ;;
-    o) INPUT_DIR=$OPTARG ;;
-    \?)
-            echo "Usage: $0 -h for help";
-            usage;
-            exit
-        ;;
-    :) echo "Option -$OPTARG requires an argument";
-       usage;
-       exit 
-       ;;
-
-  esac
-done
-
-shift $((OPTIND-1))
-
-URL=$1
+#----------------------------------------------------------------------------#
+#spinner function to display progress working indicator
+#Source:
 
 spinner()
 {
@@ -46,31 +41,57 @@ i=0
 while kill -0 $pid 2>/dev/null
 do
 i=$(( (i+1) %4))
-#printf "\r$(basename $t) ${spin:$i:1}"
 
 printf "\r${spin:$i:1} ${j}/${t_count} $filename %.4fKB          " "$size"
 sleep .1
 done
-echo -ne "\r\b>Finishing up..........."
-
+echo -ne "\r\b>Finishing up...                        "
 }
+#----------------------------------------------------------------------------#
 
-#main function
-#########----DECLARATIONS------#######
-j=0
-total_size=0
+# parse options
+while getopts 'o:hf:x:' opt ; do
+  case $opt in
+    h) usage;
+       exit
+    ;;
+    o) INPUT_DIR=$OPTARG;
+	otrue=1 ;;
+    f) fforms=$OPTARG;
+	ftrue=1 ;;
+    x) xforms=$OPTARG;
+	xtrue=1;;
+    \?) usage;
+        exit
+        ;;
+    :) echo "Option -$OPTARG requires an argument";
+       echo "Type $0 -h to display help"
+       exit
+       ;;
 
+  esac
+done
 
-#########-----------------------######
+#skip the processed options
+shift $((OPTIND-1))
 
+#make sure one and only one mandatory arg(url) is present
+if [ $# -ne 1 ]
+then
+echo "$0 missing url. Type $0 -h to display help"
+exit
+fi
 
-if [ "${URL:0:7}" != 'http://' ]
+URL=$1
+
+if [ "${URL:0:7}" != 'http://' -a "${URL:0:8}" != 'https://' ]
 then
 URL='http://'$URL
 fi
-printf "Connecting to: ${URL}"
-IFS=/ read protocol blank host query <<<"$URL";
+printf "Connecting to: ${URL}\n"
 
+#Send GET request and obtain html
+IFS=/ read protocol blank host query <<<"$URL";
 exec 3< /dev/tcp/$host/80;
 {
 echo GET /$query HTTP/1.1;
@@ -79,47 +100,83 @@ echo host: $host;
 echo
 }>&3;
 
-#file=( `sed '1,/^$/d' 0<&3|grep -oP '<img.*?>'|grep -oP 'src="\K\S+(jpg|png|jpeg)(?=")'` )
-
-file=( `sed '1,/^$/d' 0<&3|grep -oP 'src="\K\S+?(jpg|png|jpeg)(?=")'|sort -u`)
-#file=( `sed '1,/^$/d' 0<&3|egrep -o 'http://\S+(jpg|jpeg|png)'` )
-#file=(`cat arg2.txt`)
-#t_count=$(wc -w<arg2.txt)
+#Remove headers and extract image links
+file=( `sed '1,/^$/d' 0<&3|grep -oP 'src="\K\S+?(jpg|png|jpeg|gif|bmp|tif)(?=")'|sort -u|tee args`)
 
 t_count=${#file[@]}
-#setting download location
-#if [ -z "$2" ]
-#then 
-#current_dir="`pwd`"
-#path=$current_dir
-#else
+echo "Total Images Found : ${t_count}"
+
+#Customize as per options supplied
+if [ $ftrue -eq 1 ]
+then
+echo -e "Applying filter..."
+ext="$fforms"
+for t in ${ext[@]}
+do
+sed -n '/.*'"$t"'/p' args>>temp
+done
+mv temp args
+file=(`cat args`)
+t_count=$(wc -w<args)
+rm args
+fi
+
+if [ $xtrue -eq 1 ]
+then
+echo -e "Excluding specified formats..."
+ext="$xforms"
+for t in ${ext[@]}
+do
+sed -n '/.*'"$t"'/!p' args>>temp
+done
+mv temp args
+file=(`cat args`)
+t_count=$(wc -w<args)
+rm args
+fi
+
+
+echo "Downloading ${t_count} Images"
+
+#set default directory if not specified
+if [ $otrue -eq 0 ]
+then
+INPUT_DIR="`pwd`/${host}-images"
+fi
+
+#Check if directory exists, creat one if it doesn't
 if [ ! -d "$INPUT_DIR" ]
-then 
+then
 mkdir $INPUT_DIR
 fi
 path="$INPUT_DIR"
-#fi
 
 
 
+#Read each link
 
 for t in "${file[@]}"
-do 
+do
+#PRE-PROCESSING
+#Set custom filename with extension
 j=$((j+1));
-ext=$(echo $(basename $t) | sed "s/.*\(\.[^\.]*\)$/\1/");
-filename="${j}${ext}";
-#size=$(curl -sI $t|grep Content-Length|awk '{print $2}'|tr -d '\r');
+extnsn=$(echo $(basename $t) | sed "s/.*\(\.[^\.]*\)$/\1/");
+filename="${DATETIME}_IMG0${j}${extnsn}";
 
 link=$t
+
+#check for relative links and convert them to absolute links
 if [ "${link:0:4}" != "http" ]
-then 
+then
 export URL link
 export abs_link=`python -c 'import os; base=os.environ["URL"]; rel=os.environ["link"]; from urlparse import urljoin; print urljoin(str(base).strip(),str(rel).strip())'`
 else
 abs_link=$link
 fi
-printf "\rFetching file info"
 
+printf "\rFetching file info...            "
+
+#Parse headers
 IFS=/ read protocol blank host query <<<"$abs_link";
 
 exec 3< /dev/tcp/$host/80;
@@ -130,24 +187,16 @@ echo host: $host;
 echo
 }>&3;
 
+#get file size
 size=$(sed 's/.*/&/' 0<&3|grep Content-Length|awk '{print $2}'|tr -d '\r');
-#total_size=$(( total_size + $size));	
 size=$(echo "$size/1024"|bc -l)
+total_size=$(echo "$total_size + $size"|bc -l);
+
+#download image with curl and save in custom location
 (curl -s $abs_link > "${path}/${filename}") & spinner;
 done
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if [ $j -eq $t_count ]
+then
+printf "\rFinished Downloading $t_count Images (%.2fKB)\n" "$total_size"
+fi
